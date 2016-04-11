3b2d00db39b8c582ca7e724cc02523a95ed73509
fancyzhaoyue <fancyzhaoyue@gmail.com>
2016-04-11 21:35:48 +0800

    mail

--- src/com/fan/common/mail/Test.java
+++ src/com/fan/common/mail/Test.java
@@ -2,15 +2,42 @@ package com.fan.common.mail;
 
 import java.util.Properties;
 
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 
 public class Test {
-	public static void main(String[] args) {
-		Properties prop = new Properties();
-		prop.put("mail.smtp.auth", "true");
-		prop.put("mail.smtp.starttls.enable", "true");
-		prop.put("mail.smtp.host", "mail.smallpay.com");
-		prop.put("mail.smtp.port", "587");
-		Session session = Session.getInstance(prop);
+	public static void main(String[] args)  {
+		Properties props = new Properties();
+		// SMTP服务器地址
+		props.put("mail.smtp.host", "smtp.163.com");
+		// SMTP服务器端口
+		props.put("mail.stmp.port", 25);
+		// SMTP服务器是否需要用户认证
+		props.put("mail.smtp.auth", "true");
+		
+		MailAuthenticator authenticator = new MailAuthenticator("fzyue163@163.com", "Fanzhaoyue0214");
+		Session session = Session.getInstance(props, authenticator);
+		
+		MimeMessage message = new MimeMessage(session);
+		try {
+			// 主题
+			message.setSubject("注册邮件");
+			// 内容
+			message.setContent("hello", "text/html;charset=utf-8");
+			// 发件人
+			message.setFrom(new InternetAddress(authenticator.getUserName()));
+			// 收件人
+			message.setRecipient(RecipientType.TO, new InternetAddress("fancyzhaoyue@163.com"));
+			// 发送
+			Transport.send(message);
+			
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
